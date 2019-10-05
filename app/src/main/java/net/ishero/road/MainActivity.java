1631e302d5acdc32509dca7f6b6e69ff68b803cc
wmpscc <1214490813@qq.com>
2019-10-05 10:11:30 +0800

    导入包(0操作)

--- app/src/main/java/net/ishero/road/MainActivity.java
+++ app/src/main/java/net/ishero/road/MainActivity.java
@@ -1,7 +1,10 @@
 package net.ishero.road;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.ishero.road.utils.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission();
     }
+
+    private void requestPermission(){
+        Permission.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Permission.requestPermission(this, Manifest.permission.CAMERA);
+    }
+
 }
