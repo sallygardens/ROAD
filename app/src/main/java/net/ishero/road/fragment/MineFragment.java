22da8d45d2f5acec27ef4be421921d25f9370d19
wmpscc <1214490813@qq.com>
2019-10-05 11:21:23 +0800

    bottom tab

---
+++ app/src/main/java/net/ishero/road/fragment/MineFragment.java
@@ -0,0 +1,34 @@
+package net.ishero.road.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.ishero.road.R;
+
+
+public class MineFragment extends Fragment {
+
+    public MineFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mine, container, false);
+    }
+
+}
